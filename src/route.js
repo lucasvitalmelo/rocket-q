c9a6f139f7a5077b9437ba98e5f38e09be86f712
lucas vital <lucasvitaldemelo@gmail.com>
2021-07-02 15:57:42 -0300

    db error

--- src/route.js
+++ src/route.js
@@ -7,9 +7,9 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room', (req, res) => res.render("room"))
+route.get('/room/:room', (req, res) => res.render("room"))
 
-route.post('/room/:rom/:question/:action', QuestionController.index)
-route.post('/room/create-room', RoomController.create)
+route.post('/question/:rom/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
