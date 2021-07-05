130e3ce49816bd93d7f5d5109472246071c05b23
lucas vital <lucasvitaldemelo@gmail.com>
2021-07-05 15:04:34 -0300

    .

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,3 +1,5 @@
+const Database = require('../db/config')
+
 module.exports = {
   index(req, res) {
     const roomId = req.params.room;
@@ -5,6 +7,25 @@ module.exports = {
     const action = req.params.action;
     const password = req.body.password;
 
-    console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
+    console.log(`room = ${roomId}, questionId = ${questionId} 
+    action = ${action}, password = ${password}`)
+  },
+
+  async create(req, res){
+    const db = await Database()
+    const question = req.body.question
+    const roomId = req.params.room
+
+    await db.run(`INSERT INTO questions(
+      title,
+      room,
+      read
+    )VALUES(
+      "${question}",
+      ${roomId},
+      0
+    )`)
+
+    res.redirect(`/room/${roomId}`)
   }
 }
